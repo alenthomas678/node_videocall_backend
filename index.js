5dd7ed0daf24a684acbd5d8ba64b77c5c9fced41
Alen Thomas Nellary <alenthomas678@gmail.com>
2021-01-16 14:35:27 +0530

    initial commit

--- index.js
+++ index.js
@@ -6,18 +6,78 @@ app.get('/', (req, res) => {
     res.send("Node Server is running. Yay!!")
 })
 
-//Socket Logic
 const socketio = require('socket.io')(http)
 
-socketio.on("connection", (userSocket) => {
+socketio.on("connection", (socket) => {
+    console.log('connected ', socket.id);
+    var roomId = null;
 
-    userSocket.emit('connection-success', {
-        success: userSocket.id
-    })
-    userSocket.on("send_message", (data) => {
-        userSocket.broadcast.emit("receive_message", data)
-    })
-})
+    socket.emit('connection-success', {
+        success: socket.id,
+    });
+
+    socket.on("join room", (data) => {
+        roomId = data["roomID"];
+        var roomID = data["roomID"];
+        if (rooms[roomID]) {
+            const length = rooms[roomID].length;
+            if (length === 2) {
+                socket.emit("room full");
+                return;
+            }
+            rooms[roomID].push(socket.id);
+        } else {
+            rooms[roomID] = [socket.id];
+        }
+        socketToRoom[socket.id] = roomID;
+        const otherUser = rooms[roomID].find(id => id !== socket.id);
+        if (otherUser) {
+            socket.emit("online-peer", { otherUser: otherUser });
+            socket.to(otherUser).emit("joined peers", { otherUser: socket.id });
+        }
+    });
+
+    socket.on('disconnect', () => {
+        const roomID = socketToRoom[socket.id];
+        let room = rooms[roomID];
+        if (room) {
+            room = room.filter(id => id !== socket.id);
+            rooms[roomID] = room;
+        }
+        console.log(`${socket.id} has disconnected!`);
+        delete socketToRoom[socket.id];
+    });
+
+    socket.on('offer', function (data) {
+        const otherUser = rooms[roomId].find(id => id !== socket.id);
+        if (otherUser) {
+            socket.to(otherUser).emit("offer", {
+                sdp: data["sdp"],
+                socketID: data["local"]
+            });
+        }
+    });
+
+    socket.on('answer', function (data) {
+        const otherUser = rooms[roomId].find(id => id !== socket.id);
+        if (otherUser) {
+            socket.to(otherUser).emit("answer", {
+                sdp: data["sdp"],
+                socketID: data["local"]
+            });
+        }
+    });
+
+    socket.on('candidate', (data) => {
+        const otherUser = rooms[roomId].find(id => id !== socket.id);
+        if (otherUser) {
+            socket.to(otherUser).emit("candidate", {
+                candidate: data["candidate"],
+                socketID: data["local"],
+            });
+        }
+    });
+});
 
 http.listen(process.env.PORT)
 
